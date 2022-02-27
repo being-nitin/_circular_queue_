2cba5e1f19ccdc2d266d3f6a4581e363e4dda7f2
Nitin <nsinghboss02@gmail.com>
2022-02-27 09:47:37 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,24 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+	/*
+     Why circular queue is needed?
+
+     If the rear reaches to the end position of the Queue then there might be possibility that
+     some vacant spaces are left in the beginning which cannot be utilized. So, to
+     overcome such limitations, the concept of the circular queue was introduced.
+	 */
+        circularQueue cr = new circularQueue();
+        cr.insert(90);
+        cr.insert(91);
+        cr.insert(92);
+        cr.insert(93);
+        cr.insert(94);
+        cr.display();
+        System.out.println(cr.remove());
+        cr.insert(98);
+        cr.display();
+    }
+}
---
+++ src/com/Recursion/circularQueue.java
@@ -0,0 +1,61 @@
+package com.Recursion;
+import java.util.*;
+
+public class circularQueue {
+    protected int[] data;
+    private static final int default_size = 10;
+
+    protected int end =0;
+    protected int front =0;
+    private int size =0;
+
+    public circularQueue(){
+        this(default_size);
+    }
+    public circularQueue(int size){
+        this.data = new int[size];
+    }
+    public boolean isFull(){
+        return size==data.length;
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+    public boolean insert(int item){
+        if(isFull()){
+            return false;
+        }
+        data[end++]=item;
+        end= end% data.length;
+        size++;
+        return true;
+    }
+    public int remove() throws Exception{
+        if(isEmpty()){
+            throw new Exception("queue is empty");
+        }
+        int removed = data[front++];
+        front = front%data.length;
+        size--;
+        return removed;
+    }
+    public int front() throws Exception{
+        if(isEmpty()){
+            throw new Exception("queue is empty");
+        }
+        return data[front];
+    }
+    public void display(){
+        if(isEmpty()){
+            System.out.println("queue is empty");
+            return;
+        }
+        int i = front;
+        do{
+            System.out.print(data[i]+" ");
+            i++;
+            i%=data.length;
+        }while (i!=end);
+            System. out.println("END");
+    }
+}
